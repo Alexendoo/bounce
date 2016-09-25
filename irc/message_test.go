96ff88e5660d8910fd9d4836bcd21e1a245de9c0
Alex Macleod <alex@macleod.io>
2016-09-25 14:07:26 +0100

    Add tests for message parsing

---
+++ irc/message_test.go
@@ -0,0 +1,125 @@
+//    Copyright 2016 Alex Macleod
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+package irc
+
+import (
+	"testing"
+
+	c "github.com/smartystreets/goconvey/convey"
+)
+
+func TestParsing(t *testing.T) {
+	c.Convey("Parses raw messages", t, func() {
+		c.Convey("basic command", func() {
+			msg := ParseMessage("PRIVMSG")
+			c.So(msg, c.ShouldResemble, &Message{
+				tags:    map[string]string(nil),
+				prefix:  "",
+				command: "PRIVMSG",
+				params:  []string(nil),
+				time:    msg.time,
+			})
+		})
+		c.Convey("basic tag", func() {
+			msg := ParseMessage("@key=value PING")
+			c.So(msg, c.ShouldResemble, &Message{
+				tags: map[string]string{
+					"key": "value",
+				},
+				prefix:  "",
+				command: "PING",
+				params:  []string(nil),
+				time:    msg.time,
+			})
+		})
+		c.Convey("multiple tags", func() {
+			msg := ParseMessage("@first;second=2;third PING")
+			c.So(msg, c.ShouldResemble, &Message{
+				tags: map[string]string{
+					"first":  "",
+					"second": "2",
+					"third":  "",
+				},
+				prefix:  "",
+				command: "PING",
+				params:  []string(nil),
+				time:    msg.time,
+			})
+		})
+		c.Convey("prefix", func() {
+			msg := ParseMessage(":irc.example.org PING")
+			c.So(msg, c.ShouldResemble, &Message{
+				tags:    map[string]string(nil),
+				prefix:  "irc.example.org",
+				command: "PING",
+				params:  []string(nil),
+				time:    msg.time,
+			})
+		})
+		c.Convey("basic param", func() {
+			msg := ParseMessage("PING one")
+			c.So(msg, c.ShouldResemble, &Message{
+				tags:    map[string]string(nil),
+				prefix:  "",
+				command: "PING",
+				params:  []string{"one"},
+				time:    msg.time,
+			})
+		})
+		c.Convey("multiple params", func() {
+			msg := ParseMessage("PRIVMSG #channel :a b c ")
+			c.So(msg, c.ShouldResemble, &Message{
+				tags:    map[string]string(nil),
+				prefix:  "",
+				command: "PRIVMSG",
+				params: []string{
+					"#channel",
+					"a b c ",
+				},
+				time: msg.time,
+			})
+		})
+	})
+}
+
+func TestNextToken(t *testing.T) {
+	c.Convey("Calculates token offsets", t, func() {
+		c.Convey("initial offset", func() {
+			trail, lead := nextToken("ABC DEF GHI", 0, 0)
+			c.So(trail, c.ShouldEqual, 0)
+			c.So(lead, c.ShouldEqual, 3)
+		})
+		c.Convey("middle offset", func() {
+			trail, lead := nextToken("ABC DEF GHI", 0, 3)
+			c.So(trail, c.ShouldEqual, 4)
+			c.So(lead, c.ShouldEqual, 7)
+		})
+		c.Convey("penultimate offset", func() {
+			trail, lead := nextToken("ABC DEF GHI", 4, 7)
+			c.So(trail, c.ShouldEqual, 8)
+			c.So(lead, c.ShouldEqual, 11)
+		})
+		c.Convey("last offset", func() {
+			trail, lead := nextToken("ABC DEF GHI", 8, 11)
+			c.So(trail, c.ShouldEqual, 11)
+			c.So(lead, c.ShouldEqual, 11)
+		})
+		c.Convey("skips multiple spaces", func() {
+			trail, lead := nextToken("ABC  DEF", 0, 4)
+			c.So(trail, c.ShouldEqual, 5)
+			c.So(lead, c.ShouldEqual, 8)
+		})
+	})
+}
